ab6649dedfed6f1aef470ddf50935d38242063a9
Viismile <ciel3011@gmail.com>
2019-10-20 14:09:06 +0700

    Menambahkan latihan 2

---
+++ Python_Latihan2.py
@@ -0,0 +1,11 @@
+a=input("masukkan nilai a:")
+b=input("masukkan nilai b:")
+print("variable a=", a)
+print("variable b=", b)
+print("hasil penggabungan {0}&{1}=%s".format(a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {0}+{1}=%d".format(a,b) %(a+b))
+print("hasil pembagian {0}/{1}=%d".format(a,b) %(a/b))
