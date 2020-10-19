6a68fdfc0f6bfb17e1ee9f00705023199f782752
Viismile <ciel3011@gmail.com>
2020-10-19 13:24:13 +0700

    Merapihkan

--- Python_Latihan2.py
+++ Python_Latihan2.py
@@ -1,11 +1,11 @@
-a=input("masukkan nilai a:")
-b=input("masukkan nilai b:")
-print("variable a=", a)
-print("variable b=", b)
-print("hasil penggabungan {0}&{1}=%s".format(a,b) %(a+b))
+a = input("Masukkan nilai a:")
+b = input("Masukkan nilai b:")
+print("Variable a=", a)
+print("Variable b=", b)
+print("Hasil penggabungan {0}&{1}= %s".format(a, b) % (a+b))
 
-#konversi nilai variable
-a=int(a)
-b=int(b)
-print("hasil penjumlahan {0}+{1}=%d".format(a,b) %(a+b))
-print("hasil pembagian {0}/{1}=%d".format(a,b) %(a/b))
+# konversi nilai variable
+a = int(a)
+b = int(b)
+print("Hasil penjumlahan {0}+{1}= %d".format(a, b) % (a+b))
+print("Hasil pembagian {0}/{1}= %d".format(a, b) % (a/b))
